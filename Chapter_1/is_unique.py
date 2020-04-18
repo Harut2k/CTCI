ae27940ce8d1157442dd82cdd0a48049a4e6357e
Harut <45979181+Harut2k@users.noreply.github.com>
2020-04-17 18:16:52 -0700

    Create is_unique.py

---
+++ Chapter_1/is_unique.py
@@ -0,0 +1,31 @@
+def repeats_datastruct(string):
+    dict = {}
+    for i in range(0,len(string)):
+        if string[i] in dict:
+            return True
+        else:
+            dict[string[i]]=1
+    return False
+
+
+def repeats_loop(string):
+    for i in range(0,len(val)-1):
+        for j in range (i+1,len(val)):
+            if (string[i] ==string[j]):
+                return True
+    return False
+
+
+
+val = input("enter string")
+
+if(repeats_datastruct(val)):
+    print("The string contains repeating characters, used function with data structure")
+else:
+    print("The string does not contain repeating characters, used function with data structure")
+
+
+if(repeats_loop(val)):
+    print("The string contains repeating characters, used function without data structure")
+else:
+    print("The string does not contain repeating characters, used function without data structure")
